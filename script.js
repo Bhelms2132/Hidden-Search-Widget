fdd5a51c5272cc277b4ba8d5eeda6578b30ab6b0
Brian Helms <bhelms2132@gmail.com>
2021-09-07 20:01:51 -0400

    Event Listener added to button in js file

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 const search = document.querySelector('.search')
 const input = document.querySelector('.input')
 const btn = document.querySelector('.btn')
+
+//---Event Listners
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
